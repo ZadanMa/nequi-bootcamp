a2ca150789ae6bdf5257fc290a2197143f233adb
Zadan <adanmoreto14@gmail.com>
2025-05-06 21:00:52 -0500

    Aee

--- src/test/java/proyecto/nequi/bootcamps/FranquiciasApplicationTests.java
+++ src/test/java/proyecto/nequi/bootcamps/FranquiciasApplicationTests.java
@@ -1,13 +1,14 @@
 package proyecto.nequi.bootcamps;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FranquiciasApplicationTests {
-
     @Test
     void contextLoads() {
     }
-
 }
